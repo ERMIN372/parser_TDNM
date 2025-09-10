c6a283379cd8e4df514df4926e187d47e4d197e2
Iwken <74311966+ilyamerk@users.noreply.github.com>
2025-09-10 17:58:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import subprocess, sys
 from pathlib import Path
 
-QUERY = "Бариста"
+QUERY = "Оператор доставки"
 AREA = "1"           # 1=Москва
 PAGES = "3"
 PER_PAGE = "55"
 CITY = "Москва"      # для gorodrabot.ru
 
-OUTPUT = r"C:\Users\Merkulov.I\Documents\Парсер вакансий\Exports\Оператор Доставки.xlsx"
+OUTPUT = r"C:\Users\Merkulov.I\Documents\Парсер вакансий\Exports\Оператор доставки.xlsx"
 out_path = Path(OUTPUT)
 out_path.parent.mkdir(parents=True, exist_ok=True)
 
 subprocess.run([
     sys.executable, "parsers/run_pipeline.py",
-    "--query", QUERY, "--area", AREA,
-    "--pages", PAGES, "--per_page", PER_PAGE,
-    "--city", CITY,                    # ← добавили
+    "--query", "Оператор доставки",
+    "--area", "1",
+    "--city", "Москва",
+    "--pages", "2",
+    "--per_page", "100",
+    "--role", "оператор_доставки",   # ← вот это
     "--output", str(out_path)
 ], check=True)
+
 
 print("Готово:", out_path)
