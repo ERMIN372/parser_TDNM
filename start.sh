85c43ff031988b576dbdfe424e6284edf0815136
Gornostay <you@example.com>
2025-09-24 10:11:44 +0000

    fix(start): LF line endings + executable bit

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-export WEBAPP_PORT=5000
 #!/usr/bin/env bash
-set -euo pipefail
-PYBIN="python3"; command -v python3 >/dev/null 2>&1 || PYBIN="python"
+set -Eeuo pipefail
+PYBIN="$(command -v python3 || command -v python)"
 exec "$PYBIN" -m app.run
